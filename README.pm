7f3739ea6dee56fdf038324038f69c948b3d630f
Marcel Hecko <maco@blava.net>
2016-07-15 19:52:34 +0200

    explain how to run example code

--- README.pm
+++ README.pm
@@ -2,31 +2,6 @@ Trivial Python class to be used with Sigfox backend http API.
 
 Author: Marcel Hecko maco@blava.net
 
-Example
-=======
+Run example like this (use your own API username and password):
 
-  #!/usr/bin/python
-
-  import os
-  import sys
-  from pprint import pprint
-
-  import PySigfox.PySigfox as SF
-
-  login = os.getenv('SIGFOX_API_LOGIN')
-  password = os.getenv('SIGFOX_API_PASSWORD')
-
-  s = SF.PySigfox(login, password)
-
-  try:
-      s.login_test()
-      print("API login OK")
-  except Exception as e:
-      print(str(e))
-      sys.exit(1)
-
-  for device_type_id in s.device_types_list():
-      pprint(s.device_list(device_type_id))
-
-  pprint(s.device_list())
-    
+SIGFOX_API_LOGIN='5787d7bf3cxxx9106c93c795' SIGFOX_API_PASSWORD='b8faf16exxx11dd1bb1789cb25028e3b' ./example.py
